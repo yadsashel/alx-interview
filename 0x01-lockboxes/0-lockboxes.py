e31feceaf05d6d9a5864382bb67589674c97a0b7
yadsashel <ysalhi600@gmail.com>
2024-10-09 11:12:09 +0000

    The code is done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    # A list to keep track of which boxes are opened
+    
     opened = [False] * len(boxes)
-    opened[0] = True  # The first box is always opened
+    opened[0] = True  
 
-    # A list to keep track of keys to check
-    keys = boxes[0]  # Start with keys from the first box
+    keys = boxes[0]  
 
     while keys:
-        key = keys.pop()  # Take a key from the list
-        # If the key is valid and the box is not already opened
+        key = keys.pop()
+        
         if key < len(boxes) and not opened[key]:
-            opened[key] = True  # Mark the box as opened
-            keys.extend(boxes[key])  # Add keys from the newly opened box
+            opened[key] = True  
+            keys.extend(boxes[key]) 
 
-    # Check if all boxes are opened
     return all(opened)
 
-# Example usage
 boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # True
+print(canUnlockAll(boxes1)) 
 
 boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # True
+print(canUnlockAll(boxes2))  
 
 boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # False
+print(canUnlockAll(boxes))
