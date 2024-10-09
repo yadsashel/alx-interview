784eb7b08060f0fb51cc231c6cd9ec92a66ae7dc
yadsashel <ysalhi600@gmail.com>
2024-10-09 11:14:40 +0000

    The code is done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,12 +17,3 @@ def canUnlockAll(boxes):
             keys.extend(boxes[key]) 
 
     return all(opened)
-
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1)) 
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
