00ea421c1e963f0791115edd843e3e68b3b19f20
yadsashel <ysalhi600@gmail.com>
2024-10-09 08:03:51 -0700

    Resolved merge conflict by keeping remote version

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,20 +2,19 @@
 
 """ a method that determines if all the boxes can be opened """
 
-=======
-def canUnlockAll(boxes):
-    
-    opened = [False] * len(boxes)
-    opened[0] = True  
 
-    keys = boxes[0]  
+def canUnlockAll(boxes):
+
+    opened = [False] * len(boxes)
+    opened[0] = True
+
+    keys = boxes[0]
 
     while keys:
         key = keys.pop()
-        
+
         if key < len(boxes) and not opened[key]:
-            opened[key] = True  
-            keys.extend(boxes[key]) 
->>>>>>> 784eb7b08060f0fb51cc231c6cd9ec92a66ae7dc
+            opened[key] = True
+            keys.extend(boxes[key])
 
     return all(opened)
